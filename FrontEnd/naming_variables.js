52142dc83bf5e30ee55aa1b61382288407dddcc2
addleonel <cambroni3633@gmail.com>
2020-12-25 12:57:45 -0500

    Naming variables

---
+++ FrontEnd/naming_variables.js
@@ -0,0 +1,70 @@
+// Rules for Naming Variables
+// --------------------------
+// - The first character must be a letter or an underscore (_). 
+//   You can't use a number as the first character.
+
+// - The rest of the variable name can include any letter, any number, or the underscore. 
+//   You can't use any other characters, including spaces, symbols, and punctuation marks.
+
+// - As with the rest of JavaScript, variable names are case sensitive. 
+//   That is, a variable named Interest_Rate is treated as an entirely different variable than one named interest_rate.
+
+// - There's no limit to the length of the variable name.
+
+// - You can't use one of JavaScript's reserved words as a variable name. 
+//   All programming languages have a supply of words that are used internally by the language and 
+//   that can't be used for variable names because doing so would cause confusion (or worse). 
+//   Note, too, that JavaScript also has many keywords that should be avoided as well.
+
+// Case Styles: Camel, Pascal, Snake, and Kebab Case
+// -------------------------------------------------
+
+// Using snake_case
+var string_one = "this is a string";
+var string_two = "this is a string";
+
+var positive_number = 23.23;
+var negative_number = -121.32;
+
+var from_peru = true;
+var from_france = false;
+
+var fibonaci_number = [1, 1, 2, 3, 5, 8, 13, 21];
+
+var my_object = {
+	"username": "anybody",
+	"score": 14.3,
+	"proffesional":true, 
+}
+
+// Using camelCase
+var varVariable = "this is a string";
+ 
+let letVaribale = "this is a string";
+
+const piValue = 3.14159265358979;
+
+// Using PascalCase
+let NameAge = "Leonel21";
+
+// Using kebab-case
+let top-book = "Ubuntu"
+
+
+// This is a simple comment
+/*
+	This is a extend comment
+*/
+
+
+// RUN
+
+// Using console.log
+console.log("this is a string");
+console.log([1, 2, 3,4]);
+console.log(my_object);
+
+
+
+
+
