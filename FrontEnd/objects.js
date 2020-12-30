93f5d3dd55cf6a46d2a5891b630901605240004a
addleonel <cambroni3633@gmail.com>
2020-12-30 13:18:29 -0500

    Objects

---
+++ FrontEnd/objects.js
@@ -0,0 +1,39 @@
+// Objects
+// Javascript Objects are containers for named value
+// called properties or methods
+
+let person = {
+	// Properties
+	// The name:values pairs in javascript objects are called properties
+	name: "Leonel",
+	age: 21,
+	bio: "I'm computer scientist",
+	city: "London",
+	country: "Ingland",
+	email: "adleonel3633@gmail.com",
+	// Methods
+	// Methods are actions that can be performed on objects.
+	post: function(title, content){
+		let author = this.name;  // 'this' statement refers to the same object
+		let post = "Author: " + author + "\n" + 
+				   title.toUpperCase() + "\n" + content;
+		return post;
+	},
+	photo: function(photoUrl){
+		return photoUrl;
+	},
+	whoIAm: function(){
+		return "My name is " + this.name + " and I'm " + this.age + " years old";	
+	}
+}
+
+// Call Properties
+console.log(person.name);
+// or 
+console.log(person["name"]);
+
+// call Methods
+console.log(person.post("Python", "This programming language is amazing"));
+console.log(person.whoIAm());
+
+
