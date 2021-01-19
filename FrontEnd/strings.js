2ebb2f13525fd88aedb8416c759b7fcd2de22dfe
addleonel <cambroni3633@gmail.com>
2021-01-19 16:45:10 -0500

    search method

--- FrontEnd/strings.js
+++ FrontEnd/strings.js
@@ -3,6 +3,23 @@
 let message = "This is a simple message";
 
 // length of a string
-console.log(message.length);
+console.log(message.length);  // 24
 
-console.log(message.indexOf("is"));  // 2
+// The search() method cannot take a second start position argument.
+// The indexOf() method cannot take powerful search values (regular expressions).
+
+// Find a string in a string
+console.log(message.indexOf("is"));  // 2, return -1 if there isn't
+
+// Accept a second parameter as the starting position for the search
+console.log(message.indexOf("is", 3));  // 5, return -1 if there isn't 
+
+// Find the position of the last occurrence of a specified text
+console.log(message.lastIndexOf("s"));  // 20, return -1 if there isn't
+
+// Accept a second parameter as the starting position for the search
+console.log(message.lastIndexOf("s", 5));  // 3, return -1 if there isn't 
+
+// Search a string for a specified value and return the first occurrences
+console.log(message.search("simple"));  // 10, return -1 if there isn't
+
