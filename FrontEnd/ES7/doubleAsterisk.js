7ad822649b4c03684c64fd087b6e65649c8a05f0
addleonel <cambroni3633@gmail.com>
2021-06-27 20:25:22 -0500

    ES7

---
+++ FrontEnd/ES7/doubleAsterisk.js
@@ -0,0 +1,6 @@
+// DOBLE ASTERISK
+
+let base = 12;
+let exponent = 3;
+
+console.log(base ** exponent);
---
+++ FrontEnd/ES7/includeMethod.js
@@ -0,0 +1,9 @@
+// INCLUDE METHOD
+
+let myNumbers = [3, 5, 8, 1, 9];
+
+if(myNumbers.includes(10)){
+	console.log("It's here");
+}else{
+	console.log("It's not here.");
+}
