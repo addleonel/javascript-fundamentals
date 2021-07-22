bd0683fbeb525ad92a95aae244ac0ff2739b7f2a
addleonel <cambroni3633@gmail.com>
2021-07-22 10:37:35 -0500

    promise details

--- FrontEnd/ES6/promise.js
+++ FrontEnd/ES6/promise.js
@@ -1,17 +1,18 @@
 // PROMISE IN ECMAScript 6
 
 const myPromise = new Promise((resolve, reject) =>{
-	setTimeout(() => {
-		resolve("YES!");
-	}, 3000);
+	(true)
+	? setTimeout(() => resolve("Yay!"), 3000)
+	: reject(new Error("An error occurred"))
 });
 
 myPromise
-	.then(value => {return `${value} and bar`})
-	.then(value => {return `${value} and bar again`})
-        .then(value => {return `${value} and again`})
-	.then(value => {return `${value} and again`})
-	.then(value => console.log(value))
-	.catch(err => console.log(err))
+	.then(value => {return `${value} and bar.\n`})
+	.then(value => {return `${value} and bar again.\n`})
+    .then(value => {return `${value} and again.\n`})
+	.then(value => {return `${value} and again.\n`})
+	.then(value => console.log('Show value:', value))
+	.catch(err => console.log('Show', err))
+	.finally(finallyMessage => console.log('Process finished!'))
 
 	
