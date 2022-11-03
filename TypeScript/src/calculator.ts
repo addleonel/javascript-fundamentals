2a65032bd815fc27197f4177af1cc4ecf1a4212f
addleonel <adleonel3633@gmail.com>
2022-11-03 10:33:58 -0500

    control arguments

--- TypeScript/src/calculator.ts
+++ TypeScript/src/calculator.ts
@@ -14,5 +14,17 @@ const calculator = (a: number, b: number, operation: Joperation): result => {
     throw new Error('This operation is not correct');
 };
 
+const controlarguments  = (arg: Array<string>): Array<number> => {
+    if (arg.length !== 4) throw new Error('Wrong number of arguments');
+    const number1 = Number(arg[2]);
+    const number2 = Number(arg[3]);
+    if (!isNaN(number1) && !isNaN(number2)) return [number1, number2];
+    throw new Error('Provided arguments are not number!');
+};
 
-console.log(calculator(4, 5, 'divide'));
+const numbers = controlarguments(process.argv);
+
+const a = numbers[0];
+const b = numbers[1];
+
+console.log(calculator(a, b, 'divide'));
